e31547549e0f3712b6ba6f4f8b4c4357507c080d
northsum <noth24idd@gmail.com>
2024-06-07 14:02:19 +0900

    [add] try catch

--- server.py
+++ server.py
@@ -32,29 +32,33 @@ def read_root():
 # }
 @app.post("/battle", status_code=200)
 def battle(body: dict, response: Response):
-    board = body['board']
-    turn = body['turn']
+    try:
+        board = body['board']
+        turn = body['turn']
 
-    # {'UserID': 'Sample', 'slot': 0}
-    c_program = requests.post(get_user_program_url, json={'UserID':  body['c_id'], 'slot': body['c_slot']})
-    h_program = requests.post(get_user_program_url, json={'UserID':  body['h_id'], 'slot': body['h_slot']})
+        # {'UserID': 'Sample', 'slot': 0}
+        c_program = requests.post(get_user_program_url, json={'UserID':  body['c_id'], 'slot': body['c_slot']})
+        h_program = requests.post(get_user_program_url, json={'UserID':  body['h_id'], 'slot': body['h_slot']})
 
-    if not (c_program.ok and h_program.ok):
+        if not (c_program.ok and h_program.ok):
+            response.status_code = 400
+            return "Failed to get user program"
+        
+        ch_program = [c_program.json(), h_program.json()]
+        ch_script = [[], []]
+        delete_files = []
+        for program, script in zip(ch_program, ch_script):
+            if program['language'] == 'python':
+                script.extend(['python3', '-c', c_program['program']])
+            elif program['language'] == 'cpp':
+                file_name = str(random.randint(0, 100000000)) + 'exec'
+                os.system(f"echo '{c_program['program']}' > {file_name}.cpp")
+                os.system(f"g++ {file_name}.cpp -o {file_name}")
+                script.extend([f'./{file_name}'])
+                delete_files.extend([file_name, f"{file_name}.cpp"])
+    except Exception as e:
         response.status_code = 400
-        return "Failed to get user program"
-    
-    ch_program = [c_program.json(), h_program.json()]
-    ch_script = [[], []]
-    delete_files = []
-    for program, script in zip(ch_program, ch_script):
-        if program['language'] == 'python':
-            script.extend(['python3', '-c', c_program['program']])
-        elif program['language'] == 'cpp':
-            file_name = str(random.randint(0, 100000000)) + 'exec'
-            os.system(f"echo '{c_program['program']}' > {file_name}.cpp")
-            os.system(f"g++ {file_name}.cpp -o {file_name}")
-            script.extend([f'./{file_name}'])
-            delete_files.extend([file_name, f"{file_name}.cpp"])
+        return str(e)
     
     res = play_game(ch_script[0], ch_script[1], board, turn)
     for file in delete_files:
